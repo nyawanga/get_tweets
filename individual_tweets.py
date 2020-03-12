2b5420788ffe0af10672aa4283b67e0564151a64
nyawanga <francodhi@gmail.com>
2020-03-12 03:42:06 -0700

    changed requirements.txt

--- individual_tweets.py
+++ individual_tweets.py
@@ -1,8 +1,8 @@
 import sys
 
 sys.path.append("./lib")
-from lib import save_data
-from lib import get_auth_file
+from lib import save_data       # modulke to help save data to csv, excel or gsheets
+from lib import get_auth_file   # get the file with the authentication
 
 import tweepy
 from tweepy.streaming import StreamListener
@@ -18,35 +18,48 @@ auth = get_auth_file.get_auth()     #get authentication from our imported module
 start_date = datetime(2020, 3, 1, 0, 0, 0)
 end_date = datetime(2020, 3, 15, 0, 0, 0)
 tweets = []
-name = "SirHigiChege"
-tweet_count = 2
+name = "SafaricomPLC" #SirHigiChege"
+tweet_count = 20
+pages = 1
 
-def get_tweets(name,auth, start_date, end_date, limit=20, pages=1):
+def get_tweets(name,auth,start_date,end_date,limit=20,pages=1):
     # results = auth.user_timeline(id=name, count=limit)
     page_count = 0
-    for page in tweepy.Cursor(auth.user_timeline, id=name, count=limit).pages(pages):
-        page_count += 1
-        print(f"working on page: {page_count}")
-        tweets = []
-        for result in page:
-            tweet = result._json
-            text = tweet.get("text")
-            created_at = datetime.strptime(tweet.get("created_at"), "%a %b %y %H:%M:%S +0000 %Y")
+    try:
+        for page in tweepy.Cursor(auth.user_timeline, id=name, count=limit).pages(pages):
+            page_count += 1
+            print(f"working on page: {page_count}")
+            tweets = []
+            for result in page:
+                tweet = result._json
+                text = tweet.get("text")
+                created_at = datetime.strptime(tweet.get("created_at"), "%a %b %y %H:%M:%S +0000 %Y")
 
-            if created_at > start_date and created_at < end_date:
-                data = [created_at, text]
-                tweets.append(data)
+                if created_at > start_date and created_at < end_date:
+                    data = [created_at, text]
+                    tweets.append(data)
 
-        yield tweets
+            print(f"got {len(tweets)} tweets from page : {page_count}")
+            yield tweets
 
-        # control throttling
-        print("sleeping for 10 seconds...")
-        time.sleep(10)
+            # control throttling
+            print("sleeping for 10 seconds...")
+            time.sleep(10)
             # print(created_at, tweet.get("text"))
+    except Exception as err:
+        print(f"got an error")
+    finally:
+        print("exiting program")
 
-tweets = get_tweets(name,auth, start_date, end_date, limit=20, pages=2)
+
+# instantiate the generator
+tweets = get_tweets(name,auth, start_date, end_date, limit=tweet_count, pages=pages)
+
 
 for tweet in tweets:
-    print("saving data ...")
-    save_data.to_csv(tweet, "./data/csv_tweets.csv", "w")
+    if len(tweet) > 0 :
+        print("saving data ...")
+        save_data.to_csv(tweet, "./data/csv_tweets.csv", "a")
+    else:
+        print("no data got back from the request")
 
